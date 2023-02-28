d4c5d43943ed015d2bc5fecd41a1e1c0aebbab78
HiroTsune <hiroyukitsune4@gmail.com>
2023-02-27 16:24:54 -0800

    added

--- loginRegister/index.js
+++ loginRegister/index.js
@@ -1,5 +1,5 @@
 // attrは、HTML 要素の様々な属性の値を取得や変更ができるメソッド
-// showTab is variable??
+// showTab is that tab shows or hidden 
 
 $(".registerButton").click(function (event) {
     showTab(event, $(this).attr("data-tab"));
@@ -30,6 +30,7 @@ $("#login").submit(function (event) {
         $("#passwordError").text("Please enter a password which is bigger than 8 characters");
     }
 
+    // 何をしている??
     if (validateEmail(email) && password.length > 8) {
         $("#emailError").text("");
         $("#passwordError").text("");
@@ -41,8 +42,59 @@ $("#login").submit(function (event) {
 
 });
 
+/*
 
-// ??
+register時にエラーが表示されるようにする。
+
+*/
+
+$("#register").submit(function(event){
+
+    // If something isn't correct then cancel
+    event.preventDefault();
+
+    let fullName = $("#registerName").val();
+    let email = $("#registerEmail").val();
+    let password = $("#registerPassword").val();
+    let conformPassword = $("#conformPassword").val();
+
+    // Check name
+    if (!validateFullName(fullName)) {
+        $("#fullNameError").text("Please enter a valid character");
+        
+    }
+
+    // check email
+    if (!validateEmail(email)) {
+        $("#emailError").text("Please enter a valid email address");
+    }
+
+    // check password
+    if (!validatePassword(password.length < 8 )) {
+        $("#passwordError").text("Please enter a password which is bigger than 8");
+    }
+
+    // check conform password
+    if (!validateconformPassword(conformPassword === password)) {
+        $("#conformPasswordError").text("Please enter a same password");
+    }
+
+    if (validateFullName(fullName) && validateEmail(email) && password.length > 8 && conformPassword === password){
+        $("#fullNameError").text("");
+        $("#emailError").text("");
+        $("#passwprdError").text("");
+        $("#conformPasswordError").text("");
+        console.log({
+            fullName,
+            email,
+            password,
+            conformPassword,
+        });
+    } 
+});
+
+
+// /\S+@\S+\.\S+/はメールでエラーがなかった場合に表示
 function validateEmail(email) {
     let regex = /\S+@\S+\.\S+/;
     return regex.test(email);
