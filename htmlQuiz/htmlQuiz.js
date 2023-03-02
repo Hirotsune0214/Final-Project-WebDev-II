aaf06ebbe4738e6b20afcf4b3f426c7e74d9a6e7
Peng Liu <pan781026@gmail.com>
2023-03-01 20:10:53 -0800

    Fix answers show undefined.

--- htmlQuiz/htmlQuiz.js
+++ htmlQuiz/htmlQuiz.js
@@ -10,30 +10,27 @@ let htmlHardQuiz = [];
 
 let answersArr = [];
 
-//let globalArr = [];
-
 const HTML_EASY_QUIZ_URL = `${config.baseUrl}questions?apiKey=${config.apiKey}&difficulty=Easy&limit=10&tags=${config.tags}`;
 const HTML_MEDIUM_QUIZ_URL = `${config.baseUrl}questions?apiKey=${config.apiKey}&difficulty=Medium&limit=10&tags=${config.tags}`;
 const HTML_HARD_QUIZ_URL = `${config.baseUrl}questions?apiKey=${config.apiKey}&difficulty=Hard&limit=10&tags=${config.tags}`;
 
-window.addEventListener("load", async () => {
-  try {
-    //htmlEasyQuiz = await getQuestions(HTML_EASY_QUIZ_URL);
-    htmlMediumQuiz = await getQuestions(HTML_MEDIUM_QUIZ_URL);
-    htmlHardQuiz = await getQuestions(HTML_HARD_QUIZ_URL);
+// window.addEventListener("load", async () => {
+//   try {
+//     htmlEasyQuiz = await getQuestions(HTML_EASY_QUIZ_URL);
+//     htmlMediumQuiz = await getQuestions(HTML_MEDIUM_QUIZ_URL);
+//     htmlHardQuiz = await getQuestions(HTML_HARD_QUIZ_URL);
 
-    generateBannerUI();
-  } catch (err) {
-    console.log(err);
-  }
-});
+//     //generateBannerUI();
+//   } catch (err) {
+//     console.log(err);
+//   }
+// });
 
 const getQuestions = async (url, keyword) => {
   return await axios.get(url);
 };
 
-const generateBannerUI = async () => {
-  const testDiv = document.getElementById("testDiv");
+const generateEasyUI = async () => {
   htmlEasyQuiz = await getQuestions(HTML_EASY_QUIZ_URL);
   //console.log(htmlEasyQuiz);
   const { data } = htmlEasyQuiz;
@@ -43,19 +40,17 @@ const generateBannerUI = async () => {
 
   //console.log(answersArr);
 
-  // answers.filter((item, index) => answers.indexOf(item) === index);
-
   const mainContainer = document.getElementById("testDiv");
   mainContainer.innerHTML = "";
-  data.forEach(({ question, correct_answer }) => {
+  data.forEach(({ question, correct_answer, answers }) => {
     // console.log(answersArr);
 
     mainContainer.innerHTML += `
                 <section class="questionAll">
                     <h1>${question}</h1>
-                    <div id="answersDiv">${correct_answer}
+                    <div id="answersDiv">
                     </div>
-                    </section>`;
+                </section>`;
   });
 
   const answersDiv = document.getElementById("answersDiv");
@@ -75,9 +70,74 @@ const generateBannerUI = async () => {
 
     console.log(answersArr);
 
-    // answers = Object.keys(answersArr[3]).map(function (_) {
-    //     return answersArr[3][_];
-    //   });
+    for (let i = 0; i < answersArr.length; i++) {
+      answersDiv.innerHTML += `
+            <button>${answersArr[i]}</button>
+            </br>`;
+    }
+  }
+
+  // answers = Object.keys(answersArr[3]).map(function (_) {
+  //     return answersArr[3][_];
+  //   });
+
+  // answersDiv.innerHTML += `
+  //           <div>${answersArr[0]}</div>
+  //           <div>${answersArr[1]}</div>
+  //           <div>${answersArr[2]}</div>
+  //           <div>${answersArr[3]}</div>
+  //           <div>${answersArr[4]}</div>
+  //           <div>${answersArr[5]}</div>
+  //           </br>`;
+};
+function easyLevel() {
+  console.log("In the easy questions");
+
+  generateEasyUI();
+}
+
+function mediumLevel() {
+  console.log("In the medium questions");
+
+  generateMediumUI();
+}
+
+function hardLevel() {
+  console.log("In the hard questions");
+
+  generateHardUI();
+}
+
+const generateMediumUI = async () => {
+  const testDiv = document.getElementById("testDiv");
+  htmlMediumQuiz = await getQuestions(HTML_MEDIUM_QUIZ_URL);
+
+  const { data } = htmlMediumQuiz;
+
+  const mainContainer = document.getElementById("testDiv");
+  mainContainer.innerHTML = "";
+  data.forEach(({ question, correct_answer }) => {
+    mainContainer.innerHTML += `
+                <section class="questionAll">
+                    <h1>${question}</h1>
+                    <div id="answersDiv">${correct_answer}
+                    </div>
+                    </section>`;
+  });
+
+  const answersDiv = document.getElementById("answersDiv");
+
+  for (x in data) {
+    answersArr = Object.values(data[x].answers);
+    console.log("aaa", answersArr);
+
+    for (let i = answersArr.length - 1; i >= 0; i--) {
+      if (answersArr[i] == null) {
+        answersArr.pop();
+      }
+    }
+
+    console.log(answersArr);
 
     answersDiv.innerHTML += `
             <div>${answersArr[0]}</div>
@@ -90,4 +150,44 @@ const generateBannerUI = async () => {
   }
 };
 
-const easyLevel = document.getElementById("easyLevel");
+const generateHardUI = async () => {
+  const testDiv = document.getElementById("testDiv");
+  htmlHardQuiz = await getQuestions(HTML_HARD_QUIZ_URL);
+
+  const { data } = htmlHardQuiz;
+
+  const mainContainer = document.getElementById("testDiv");
+  mainContainer.innerHTML = "";
+  data.forEach(({ question, correct_answer }) => {
+    mainContainer.innerHTML += `
+                <section class="questionAll">
+                    <h1>${question}</h1>
+                    <div id="answersDiv">${correct_answer}
+                    </div>
+                    </section>`;
+  });
+
+  const answersDiv = document.getElementById("answersDiv");
+
+  for (x in data) {
+    answersArr = Object.values(data[x].answers);
+    console.log("aaa", answersArr);
+
+    for (let i = answersArr.length - 1; i >= 0; i--) {
+      if (answersArr[i] == null) {
+        answersArr.pop();
+      }
+    }
+
+    console.log(answersArr);
+
+    answersDiv.innerHTML += `
+            <div>${answersArr[0]}</div>
+            <div>${answersArr[1]}</div>
+            <div>${answersArr[2]}</div>
+            <div>${answersArr[3]}</div>
+            <div>${answersArr[4]}</div>
+            <div>${answersArr[5]}</div>
+            </br>`;
+  }
+};
