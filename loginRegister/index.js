80e2dc0ccba2a7eff03dd43624414ede953479a3
HiroTsune <hiroyukitsune4@gmail.com>
2023-03-01 18:05:07 -0800

    new

--- loginRegister/index.js
+++ loginRegister/index.js
@@ -9,88 +9,134 @@ function showTab(event, tabName) {
     $(".loginButton").hide(); // hide HTML
     $(".registerButton").removeClass("loginButton"); // remove class
     // What does # mean??
-    $(`#${tabName}`).show();
+    $(`#${tabName}`).show(); // data-tab
     console.log(event.currentTarget, "current Target");
     $(event.currentTarget).addClass("loginButton");
-    // line 10 and 14
+
 }
+
+// login
 
 $("#login").submit(function (event) {
     // preventDefault - 実行したイベントがキャンセル可能である場合、イベントをキャンセルするためのメソッド
     event.preventDefault();
 
+    let lastName = $("#loginLastName").val();
+    let firstName = $("#loginFirstName").val();
     let email = $("#loginEmail").val();
     let password = $("#loginPassword").val();
 
-    if (!validateEmail(email)) {
-        $("#emailError").text("Please enter a valid email address");
-    }
 
-    if (password.length < 8) {
-        $("#passwordError").text("Please enter a password which is bigger than 8 characters");
-    }
+    if (lastName && firstName && email && password) {
+        $("h1").html(`${lastName} Welcome Back`);
+        setTimeout(function() {
+        window.location = "https://www.google.co.jp";
+    }, 3*1000);
+    } else {
 
-    // 何をしている??
-    if (validateEmail(email) && password.length > 8) {
-        $("#emailError").text("");
-        $("#passwordError").text("");
-        console.log({
-            email,
-            password,
-        });
-    }
 
+        if (!lastName) {
+            $("#lastNameError").text("Please enter appropriate characters");
+        }
+
+        if (!firstName) {
+            $("#firstNameError").text("Please enter appropriate characters");
+        }
+
+        if (!validateEmail(email)) {
+            $("#emailError").text("Please enter a valid email address");
+        }
+
+        if (password.length < 8) {
+            $("#passwordError").text("Please enter a password which is bigger than 8 characters");
+        }
+
+        if (lastName && firstName && validateEmail(email) && password.length > 8) {
+            $("#lastNameError").text("");
+            $("#firstNameError").text("");
+            $("#emailError").text("");
+            $("#passwordError").text("");
+            console.log({
+                lastName,
+                firstName,
+                email,
+                password,
+            });
+        }
+
+    }
 });
 
-/*
 
-register時にエラーが表示されるようにする。
 
-*/
 
-$("#register").submit(function(event){
+// register
+
+$("#register").submit(function (event) {
 
     // If something isn't correct then cancel
     event.preventDefault();
 
-    let fullName = $("#registerName").val();
+    let lastName = $("#registerLastName").val();
+    let firstName = $("#registerFirstName").val();
     let email = $("#registerEmail").val();
     let password = $("#registerPassword").val();
     let conformPassword = $("#conformPassword").val();
 
-    // Check name
-    if (!validateFullName(fullName)) {
-        $("#fullNameError").text("Please enter a valid character");
+    if (lastName && firstName && email && password && conformPassword) {
+        $("h1").html(`${lastName} Welcome!!`);
+
+        setTimeout(function() {
+            window.location = "https://www.google.co.jp";
+        }, 3*1000);
+    } else {
+
+        // Check name
+        if (!lastName) {
+            $("#registerLastName").text("Please enter appropriate characters");
+        }
+
+        if (!firstName) {
+            $("#registerFirstName").text("Please enter appropriate characters");
+        }
+
+        // check email
+        if (!validateEmail(email)) {
+            $("#registerEmailError").text("Please enter a valid email address");
+        }
+
+        // check password
+        if (password.length < 8) {
+            $("#registerPasswordError").text("Please enter a password which is bigger than 8");
+        }
+
+        // check conform password
+        if (conformPassword !== password) {
+            $("#conformPasswordError").text("Please enter a same password");
+        }
+
+        if (lastName && firstName && validateEmail(email) && password.length > 8 && conformPassword !== password) {
+            $("#lastNameError").text("");
+            $("#firstNameError").text("");
+            $("#registerEmailError").text("");
+            $("#registerPasswordError").text("");
+            $("#conformPasswordError").text("");
+            console.log({
+                lastName,
+                firstName,
+                email,
+                password,
+                conformPassword,
+            });
+        }
+
+        /* if a user completes register then show "Welcome last name"
+        $("h1").html(`Welcome ${lastName}`);
         
-    }
+        
+        */
 
-    // check email
-    if (!validateEmail(email)) {
-        $("#emailError").text("Please enter a valid email address");
     }
-
-    // check password
-    if (!validatePassword(password.length < 8 )) {
-        $("#passwordError").text("Please enter a password which is bigger than 8");
-    }
-
-    // check conform password
-    if (!validateconformPassword(conformPassword === password)) {
-        $("#conformPasswordError").text("Please enter a same password");
-    }
-
-    if (validateFullName(fullName) && validateEmail(email) && password.length > 8 && conformPassword === password){
-        $("#fullNameError").text("");
-        $("#emailError").text("");
-        $("#passwprdError").text("");
-        $("#conformPasswordError").text("");
-        console.log({
-            fullName,
-            email,
-            password,
-            conformPassword,
-        });
-    } 
 });
 
 
@@ -98,4 +144,14 @@ $("#register").submit(function(event){
 function validateEmail(email) {
     let regex = /\S+@\S+\.\S+/;
     return regex.test(email);
+}
+
+function lastName() {
+    let lastNameRegex = /\W/;
+    return lastNameRegex.test(lastName);
+}
+
+function firstName() {
+    let firstRegex = /\W/;
+    return firstRegex.test(firstName);
 }
